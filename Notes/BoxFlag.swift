edb98a6eb814e7fc92957d6ec042e5af66774bee
Tianid <tianid73@gmail.com>
2019-07-05 01:30:05 +0300

    added resizable text view , added flag drawed by CoreGraphics

---
+++ Notes/BoxFlag.swift
@@ -0,0 +1,69 @@
+//
+//  BoxFlag.swift
+//  Notes
+//
+//  Created by Tianid on 04/07/2019.
+//  Copyright © 2019 tianid. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class BoxFlag: UIView {
+    @IBInspectable var shapeColor: UIColor = .yellow {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    @IBInspectable var shapePosition: CGPoint = .zero {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    @IBInspectable var lineWidth: CGFloat = 1 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    @IBInspectable var shapeSize: CGSize = CGSize(width: 5, height: 5) {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    @IBInspectable var isShapeHiden: Bool = false {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        guard isShapeHiden else {
+            return
+        }
+        shapeColor.setFill()
+        
+        setFlagPosition()
+        let pathX = getX(in: CGRect(origin: shapePosition, size: shapeSize))
+        
+        pathX.fill()
+    }
+    
+    private func getX(in rect:CGRect) -> UIBezierPath {
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.lineWidth = lineWidth
+        path.close()
+        path.stroke()
+        path.fill()
+        
+        return path
+    }
+    
+   private func setFlagPosition() {
+        shapePosition = CGPoint(x: shapePosition.x, y: shapePosition.y)
+    }
+}
--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -1,13 +1,94 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var whiteBox: BoxFlag!
+    @IBOutlet weak var redBox: BoxFlag!
+    @IBOutlet weak var greenBox: BoxFlag!
+    @IBOutlet weak var coloredBox: BoxFlag!
+    
+    var boxChar: Character = " "
+    
+    @IBAction func actionWhiteBoxTapped(_ sender: Any) {
+        whiteBox.isShapeHiden = !whiteBox.isShapeHiden
+        if whiteBox.isShapeHiden {
+            redBox.isShapeHiden = false
+            greenBox.isShapeHiden = false
+            boxChar = "W"
+        } else {
+            boxChar = " "
+        }
+    }
+    
+    @IBAction func actionRedBoxTapped(_ sender: Any) {
+        redBox.isShapeHiden = !redBox.isShapeHiden
+        if redBox.isShapeHiden {
+            whiteBox.isShapeHiden = false
+            greenBox.isShapeHiden = false
+            boxChar = "R"
+        } else {
+            boxChar = " "
+        }
+    }
+    
+    @IBAction func actionGreenBoxTapped(_ sender: Any) {
+        greenBox.isShapeHiden = !greenBox.isShapeHiden
+        if greenBox.isShapeHiden {
+            redBox.isShapeHiden = false
+            whiteBox.isShapeHiden = false
+            boxChar = "G"
+        } else {
+            boxChar = " "
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        whiteBox.backgroundColor = UIColor.white
+        whiteBox.layer.borderWidth = 1
+        whiteBox.layer.borderColor = UIColor.gray.cgColor
+        
+        redBox.backgroundColor = UIColor.red
+        redBox.layer.borderWidth = 1
+        redBox.layer.borderColor = UIColor.gray.cgColor
+        
+        greenBox.backgroundColor = UIColor.green
+        greenBox.layer.borderWidth = 1
+        greenBox.layer.borderColor = UIColor.gray.cgColor
+        
+        coloredBox.backgroundColor = UIColor.orange
+        coloredBox.layer.borderWidth = 1
+        coloredBox.layer.borderColor = UIColor.gray.cgColor
+        
+        textView.backgroundColor = UIColor.white
+        textView.isScrollEnabled = false
+        textView.layer.borderWidth = 1
+        textView.layer.borderColor = UIColor.gray.cgColor
+        textView.delegate = self
+        textViewDidChange(textView)
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    public func textViewDidChange(_ textView: UITextView) {
+        let size = CGSize(width: textView.frame.width, height: .infinity)
+        let estimatedSize = textView.sizeThatFits(size)
+        textView.constraints.forEach { (constraint) in
+            if constraint.firstAttribute == .height {
+                constraint.constant = estimatedSize.height
+            }
+            
+        }
+    }
+    
+    
 
+    
 
 }
+
+
 
