6d4f5f6d472f247d2b866034065f0543528e324d
Tianid <tianid73@gmail.com>
2019-07-16 15:36:05 +0300

    [-] Added deleting notes

--- Notes/MyTableViewController.swift
+++ Notes/MyTableViewController.swift
@@ -12,16 +12,20 @@ class MyTableViewController: UIViewController {
     @IBAction func addButtonTap(_ sender: UIBarButtonItem) {
         performSegue(withIdentifier: "toEditScreen", sender: nil)
     }
+    @IBAction func editButtonTap (_ sender: UIBarButtonItem) {
+        myTableView.isEditing = !myTableView.isEditing
+    }
+    
     @IBOutlet weak var myTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         myTableView.delegate = self
         myTableView.dataSource = self
         fileNoteBook = FileNotebook()
-//        fileNoteBook?.add(Note(uid: "as", title: "1", content: "asdf", color: .green, importance: .notImportant))
-//        fileNoteBook?.add(Note(uid: "as1", title: "2", content: "asdf", color: .red, importance: .common))
-//        fileNoteBook?.add(Note(uid: "as2", title: "3", content: "asdf", color: .white, importance: .important))
-//        fileNoteBook?.add(Note(uid: "sd3", title: "4", content: "qwerty", color: .orange, importance: .important, selfDestructionDate: Date()))
+        fileNoteBook?.add(Note(uid: "as", title: "1", content: "asdf", color: .green, importance: .notImportant))
+        fileNoteBook?.add(Note(uid: "as1", title: "2", content: "asdf", color: .red, importance: .common))
+        fileNoteBook?.add(Note(uid: "as2", title: "3", content: "asdf", color: .white, importance: .important))
+        fileNoteBook?.add(Note(uid: "sd3", title: "4", content: "qwerty", color: .orange, importance: .important, selfDestructionDate: Date()))
         
         notes = (fileNoteBook?.getArrayOfNotes())!
         
@@ -81,6 +85,23 @@ extension MyTableViewController: UITableViewDelegate, UITableViewDataSource {
     override func viewWillAppear(_ animated: Bool) {
         notes = (fileNoteBook?.getArrayOfNotes())!
         myTableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            myTableView.beginUpdates()
+            let temp = notes[indexPath.row]
+            notes.remove(at: indexPath.row)
+            fileNoteBook?.remove(with: temp.uid)
+            myTableView.deleteRows(at: [indexPath], with: .automatic)
+            myTableView.endUpdates()
+//            myTableView.reloadData()
+        }
     }
     
 }
